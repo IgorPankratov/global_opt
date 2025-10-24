e09a319318d4419322abe261a5ace639ab2d0bb1
IgorP <ipankrat85@gmail.com>
2025-10-24 22:47:40 +0300

    Section 'Feedback'. Add snake carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,36 @@
-let currentIndex = 0;
-const items = document.querySelectorAll('.feedback__carousel-item');
-const inner = document.querySelector('.feedback__carousel-inner');
-const nextBtn = document.querySelector('.feedback__carousel-nextBtn');
-const prevBtn = document.querySelector('.feedback__carousel-prevBtn');
-const totalItems = items.length;
+// let currentIndex = 0;
+// const items = document.querySelectorAll('.feedback__carousel-item');
+// const inner = document.querySelector('.feedback__carousel-inner');
+// const nextBtn = document.querySelector('.feedback__carousel-nextBtn');
+// const prevBtn = document.querySelector('.feedback__carousel-prevBtn');
+// const totalItems = items.length;
 
-function slide(index) {
-    // ОБработка цикличности 
-    if (index < 0) {
-        index = totalItems - 1;
-    }  else if (index >= totalItems) {
-        index = 0;
-    }
+// function slide(index) {
+//     // ОБработка цикличности 
+//     if (index < 0) {
+//         index = totalItems - 1;
+//     }  else if (index >= totalItems) {
+//         index = 0;
+//     }
     
-    inner.style.transform = `translateX(-${index * 100}%)`;
-    currentIndex = index;
+//     inner.style.transform = `translateX(-${index * 100}%)`;
+//     currentIndex = index;
 
-     // Добавляем и удаляем класс active
-    items.forEach((item, i) => {
-        item.classList.toggle('active', i === index);
-    });
-}
+//      // Добавляем и удаляем класс active
+//     items.forEach((item, i) => {
+//         item.classList.toggle('active', i === index);
+//     });
+// }
 
-// items[0].classList.add("active");
+// // items[0].classList.add("active");
 
-nextBtn.addEventListener('click', () => {
-        slide(currentIndex + 1);    
-});
+// nextBtn.addEventListener('click', () => {
+//         slide(currentIndex + 1);    
+// });
 
-prevBtn.addEventListener('click', () => {
-        slide(currentIndex - 1);
-});
+// prevBtn.addEventListener('click', () => {
+//         slide(currentIndex - 1);
+// });
 
 // Автоматическое переключение (опционально)
 // setInterval(() => {
@@ -40,3 +40,22 @@ prevBtn.addEventListener('click', () => {
 //         slide(0);
 //     }
 // }, 3000);
+
+new Snake('.feedback__carousel', {
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '60px',
+    dots: true,
+    arrows: true,
+    responsive: [
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
